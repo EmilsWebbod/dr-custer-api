c3f32af36f918c863042bc953090bc69174efd8a
EmilsWebbod <emil@webbod.no>
2018-04-12 20:31:11 +0200

    Added Test with watcher

--- index.spec.js
+++ index.spec.js
@@ -6,3 +6,7 @@ const numFormatter = require('./index');
 test('NUMBER FORMATER', t => {
     t.is(numFormatter(1), '1');
 });
+
+test('NUMBER FORMATER 2', t => {
+    t.is(numFormatter(2333), '2,333')
+});
